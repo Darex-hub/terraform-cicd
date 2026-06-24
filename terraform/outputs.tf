3718d709c7cac3e36925b90a7dc194bf8640c49f
Darex-hub <Darex-hub>
2026-06-24 10:06:13 -0400

    update

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,14 @@
-output "bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = aws_s3_bucket.darex.bucket
+output "terraform_state_bucket_id" {
+  description = "The ID of the Terraform state S3 bucket"
+  value       = aws_s3_bucket.terraform_state.id
 }
 
-output "bucket_arn" {
-  description = "The ARN of the S3 bucket"
-  value       = aws_s3_bucket.darex.arn
+output "terraform_state_bucket_arn" {
+  description = "The ARN of the Terraform state S3 bucket"
+  value       = aws_s3_bucket.terraform_state.arn
+}
+
+output "terraform_state_bucket_name" {
+  description = "The name of the Terraform state S3 bucket"
+  value       = aws_s3_bucket.terraform_state.bucket
 }
