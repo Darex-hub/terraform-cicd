a5d7e55cefe81deb51ee1130804d31631f8ee1e1
Darex-hub <Darex-hub>
2026-06-24 10:29:39 -0400

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 # Create the S3 bucket for Terraform state
 resource "aws_s3_bucket" "terraform_state" {
-  bucket_name = "darex-terraform-state-bucket"
+  bucket_name = "${var.bucket_name}"
   acl    = "private"
 
   versioning {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,5 @@
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
+  default     = "darex-terraform-state-bucket"
 }
